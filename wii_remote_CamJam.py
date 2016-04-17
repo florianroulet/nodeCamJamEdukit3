3bb7ee2ec79bad42f1f1e6143642e717981fb167
Florian Roulet <florianroulet@gmail.com>
2016-04-17 09:47:12 +0200

    test wiimote en continu

--- wii_remote_CamJam.py
+++ wii_remote_CamJam.py
@@ -31,87 +31,91 @@ button_delay = 0.1
 
 print('Press 1 + 2 on your Wii Remote now ...')
 time.sleep(1)
-
-# Connect to the Wii Remote. If it times out
-# then quit.
-try:
-  wii=cwiid.Wiimote()
-except RuntimeError:
-  print("Error opening wiimote connection")
-  quit()
-
-print('Wii Remote connected...\n')
-print('Press some buttons!\n')
-print('Press PLUS and MINUS together to disconnect and quit.\n')
-
-wii.rpt_mode = cwiid.RPT_BTN
-
 while True:
+    # Connect to the Wii Remote. If it times out
+    # then quit.
+    try:
+      wii=cwiid.Wiimote()
+    except RuntimeError:
+      print("Error opening wiimote connection")
+      quit()
 
-  buttons = wii.state['buttons']
+    print('Wii Remote connected...\n')
+    print('Press some buttons!\n')
+    print('Press PLUS and MINUS together to disconnect and quit.\n')
 
-  # If Plus and Minus buttons pressed
-  # together then rumble and quit.
-  if (buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
-    print('\nClosing connection ...')
-    wii.rumble = 1
-    time.sleep(1)
-    wii.rumble = 0
-    exit(wii)
+    wii.rpt_mode = cwiid.RPT_BTN
 
-  # Check if other buttons are pressed by
-  # doing a bitwise AND of the buttons number
-  # and the predefined constant for that button.
-  if (buttons & cwiid.BTN_LEFT):
-    print('Left pressed')
-    response = urllib.urlopen("http://localhost:8080/backward").read()
-    time.sleep(button_delay)
-    #io.output(2, True)
+    continueProgramm = 1
 
-  if(buttons & cwiid.BTN_RIGHT):
-    print('Right pressed')
-    response = urllib.urlopen("http://localhost:8080/forward").read()
-    time.sleep(button_delay)
-    #io.output(3, True)
+    while continueProgramm:
 
-  if (buttons & cwiid.BTN_UP):
-    print('Up pressed')
-    response = urllib.urlopen("http://localhost:8080/left").read()
-    time.sleep(button_delay)
-    #io.output(4, True)
+      buttons = wii.state['buttons']
 
-  if (buttons & cwiid.BTN_DOWN):
-    print('Down pressed')
-    response = urllib.urlopen("http://localhost:8080/right").read()
-    time.sleep(button_delay)
-    #io.output(17, True)
+      # If Plus and Minus buttons pressed
+      # together then rumble and quit.
+      if (buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
+        print('\nClosing connection ...')
+        wii.rumble = 1
+        time.sleep(1)
+        wii.rumble = 0
+        #exit(wii)
+        wii.close()
+        continueProgramm = 0
 
-  if (buttons & cwiid.BTN_1):
-    print('Button 1 pressed')
-    time.sleep(button_delay)
+      # Check if other buttons are pressed by
+      # doing a bitwise AND of the buttons number
+      # and the predefined constant for that button.
+      if (buttons & cwiid.BTN_LEFT):
+        print('Left pressed')
+        response = urllib.urlopen("http://localhost:8080/backward").read()
+        time.sleep(button_delay)
+        #io.output(2, True)
 
-  if (buttons & cwiid.BTN_2):
-    print('Button 2 pressed')
-    time.sleep(button_delay)
+      if(buttons & cwiid.BTN_RIGHT):
+        print('Right pressed')
+        response = urllib.urlopen("http://localhost:8080/forward").read()
+        time.sleep(button_delay)
+        #io.output(3, True)
 
-  if (buttons & cwiid.BTN_A):
-    print('Button A pressed')
-    time.sleep(button_delay)
-    #for i in pins:
-      #io.output(i, False)
+      if (buttons & cwiid.BTN_UP):
+        print('Up pressed')
+        response = urllib.urlopen("http://localhost:8080/left").read()
+        time.sleep(button_delay)
+        #io.output(4, True)
 
-  if (buttons & cwiid.BTN_B):
-    print('Button B pressed')
-    time.sleep(button_delay)
+      if (buttons & cwiid.BTN_DOWN):
+        print('Down pressed')
+        response = urllib.urlopen("http://localhost:8080/right").read()
+        time.sleep(button_delay)
+        #io.output(17, True)
 
-  if (buttons & cwiid.BTN_HOME):
-    print('Home Button pressed')
-    time.sleep(button_delay)
+      if (buttons & cwiid.BTN_1):
+        print('Button 1 pressed')
+        time.sleep(button_delay)
 
-  if (buttons & cwiid.BTN_MINUS):
-    print('Minus Button pressed')
-    time.sleep(button_delay)
+      if (buttons & cwiid.BTN_2):
+        print('Button 2 pressed')
+        time.sleep(button_delay)
 
-  if (buttons & cwiid.BTN_PLUS):
-    print('Plus Button pressed')
-    time.sleep(button_delay)
+      if (buttons & cwiid.BTN_A):
+        print('Button A pressed')
+        time.sleep(button_delay)
+        #for i in pins:
+          #io.output(i, False)
+
+      if (buttons & cwiid.BTN_B):
+        print('Button B pressed')
+        time.sleep(button_delay)
+
+      if (buttons & cwiid.BTN_HOME):
+        print('Home Button pressed')
+        time.sleep(button_delay)
+
+      if (buttons & cwiid.BTN_MINUS):
+        print('Minus Button pressed')
+        time.sleep(button_delay)
+
+      if (buttons & cwiid.BTN_PLUS):
+        print('Plus Button pressed')
+        time.sleep(button_delay)
