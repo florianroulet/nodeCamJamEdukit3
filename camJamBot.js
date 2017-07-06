f4d567fe619a4e6c1883f0a722a7647e62391512
Florian Roulet <florianroulet@gmail.com>
2017-07-06 11:56:52 +0200

    test

--- camJamBot.js
+++ camJamBot.js
@@ -15,7 +15,7 @@ util.inherits(CamJamBot, Bot);
 
 
 CamJamBot.prototype.run = function () {
-    CamJamBot.super_.call(this, this.settings);
+    Bot.call(this, this.settings);
 
     this.on('start', this._onStart);
     this.on('message', this._onMessage);
