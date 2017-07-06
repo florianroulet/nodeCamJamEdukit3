dae64b1bfbcd2cb0285176b70629a4d2fecf2407
Florian Roulet <florianroulet@gmail.com>
2017-07-06 10:46:27 +0200

    adding slackbot

--- app.js
+++ app.js
@@ -9,6 +9,23 @@ var spawn = require("child_process").spawn;
 var pythonProcess = spawn('python', ['wii_remote_CamJam.py']);
 /***************************************** GESTION wiimote ***********************************************************/
 
+/***************************************** GESTION slackbot **********************************************************/
+var Bot = require('slackbots');
+
+// create a bot
+var settings = {
+    token: '',
+    name: 'camjamedukit3'
+};
+var bot = new Bot(settings);
+
+bot.on('start', function() {
+    bot.postMessageToChannel('some-channel-name', 'Hello channel!');
+    bot.postMessageToUser('some-username', 'hello bro!');
+    bot.postMessageToGroup('some-private-group', 'hello group chat!');
+});
+
+/***************************************** GESTION slackbot **********************************************************/
 
 
 /***************************************** GESTION page web **********************************************************/
