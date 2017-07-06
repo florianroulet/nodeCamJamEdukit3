7d19be490f93cc29ee93ccb3e3e33f775df795bc
Florian Roulet <florianroulet@gmail.com>
2017-07-06 11:46:51 +0200

    cleanup

--- camJamBot.js
+++ camJamBot.js
@@ -16,7 +16,7 @@ util.inherits(CamJamBot, Bot);
 
 
 CamJamBot.prototype.run = function () {
-    NorrisBot.super_.call(this, this.settings);
+    CamJamBot.super_.call(this, this.settings);
 
     this.on('start', this._onStart);
     this.on('message', this._onMessage);
