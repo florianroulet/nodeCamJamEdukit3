b5c4da5bfd8c9ff5c58c48d4ae554111b787c719
Florian Roulet <florianroulet@gmail.com>
2017-07-06 17:24:54 +0200

    class structure

--- camJamBot.js
+++ camJamBot.js
@@ -3,71 +3,65 @@
 var util = require('util');
 var Bot = require('slackbots');
 
-var CamJamBot = function Constructor(settings) {
-    this.settings = settings;
-    this.settings.name = this.settings.name || 'camJamBot';
+class CamJamBot extends Bot {
 
-    this.user = null;
-    CamJamBot.super_.call(this, this.settings);
+    constructor(settings) {
+        super();
+        this.settings = settings;
+        this.settings.name = this.settings.name || 'camJamBot';
 
-};
-
-// inherits methods and properties from the Bot constructor
-util.inherits(CamJamBot, Bot);
-
-
-CamJamBot.prototype.run = function () {
-
-    this.on('start', this._onStart);
-    this.on('message', this._onMessage);
-};
-
-CamJamBot.prototype._onStart = function () {
-    this._loadBotUser();
-    this._welcomeMessage();
-};
-
-CamJamBot.prototype._loadBotUser = function () {
-    var self = this;
-    this.user = this.users.filter(function (user) {
-        return user.name === self.name;
-    })[0];
-};
-
-CamJamBot.prototype._welcomeMessage = function () {
-    var params = {
-        icon_emoji: ':rocket:',
-        as_user: true
-    };
-    this.postMessageToChannel('general', 'Hello there !', params);
-};
-
-CamJamBot.prototype._onMessage = function (message) {
-    if (this._isChatMessage(message) &&
-        this._isChannelConversation(message) &&
-        !this._isFromMe(message) &&
-        this._isMentioningMe(message)
-    ) {
-        console.log(data);
+        this.user = null;
+        this.on('start', this._onStart);
+        this.on('message', this._onMessage);
     }
-};
 
-CamJamBot.prototype._isChatMessage = function (message) {
-    return message.type === 'message' && Boolean(message.text);
-};
+    _onStart = function () {
+        this._loadBotUser();
+        this._welcomeMessage();
+    };
 
-CamJamBot.prototype._isChannelConversation = function (message) {
-    return typeof message.channel === 'string' &&
-        message.channel[0] === 'C';
-};
+    _loadBotUser = function () {
+        let self = this;
+        this.user = this.users.filter(function (user) {
+            return user.name === self.name;
+        })[0];
+    };
 
-CamJamBot.prototype._isFromMe = function (message) {
-    return message.user === this.user.id;
-};
+    _welcomeMessage = function () {
+        let params = {
+            icon_emoji: ':rocket:',
+            as_user: true
+        };
+        this.postMessageToChannel('general', 'Hello there !', params);
+    };
 
-CamJamBot.prototype._isMentioningMe = function (message) {
-    return message.text.toLowerCase().indexOf('camjam') > -1 ||
-        message.text.toLowerCase().indexOf(this.name) > -1;
-};
+    _onMessage = function (message) {
+        if (this._isChatMessage(message) &&
+            this._isChannelConversation(message) &&
+            !this._isFromMe(message) &&
+            this._isMentioningMe(message)
+        ) {
+            console.log(data);
+        }
+    };
+
+    _isChatMessage = function (message) {
+        return message.type === 'message' && Boolean(message.text);
+    };
+
+    _isChannelConversation = function (message) {
+        return typeof message.channel === 'string' &&
+            message.channel[0] === 'C';
+    };
+
+    _isFromMe = function (message) {
+        return message.user === this.user.id;
+    };
+
+    _isMentioningMe = function (message) {
+        return message.text.toLowerCase().indexOf('camjam') > -1 ||
+            message.text.toLowerCase().indexOf(this.name) > -1;
+    };
+}
 
 module.exports = CamJamBot;
